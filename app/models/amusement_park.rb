cc2b63b08d43c9d1ceb94c18c15f3e9f0ae8d4d6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-03 10:49:36 -0600

    Add test and method to order amusement park rides alphabetically

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,4 +1,8 @@
 class AmusementPark < ApplicationRecord
   validates_presence_of :name, :admission_price
   has_many :rides
+
+  def sort_rides_alphabetically
+    rides.order(:name)
+  end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe AmusementPark do
+  before(:each) do
+    @elitches = AmusementPark.create!(name: "Elitches",
+                                      admission_price: "$50.00")
+    @ride1 = @elitches.rides.create!(name: "Batman",
+                                    thrill_rating: 6)
+    @ride2 = @elitches.rides.create!(name: "Superman",
+                                    thrill_rating: 10)
+    @ride3 = @elitches.rides.create!(name: "Petting Zoo",
+                            thrill_rating: 1)
+  end
+
   describe 'relationships' do
     it {should have_many :rides}
+  end
+
+  describe 'instance methods' do
+    it "#sort_rides_alphabetically" do
+      expect(@elitches.sort_rides_alphabetically).to eq([@ride1, @ride3, @ride2])
+    end
   end
 end
