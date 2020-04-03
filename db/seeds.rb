57349aa51547737aaa318ca986879130d0aa6c09
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-03 10:25:13 -0600

    Add mechanics to seed file for browser testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Mechanic.destroy_all
+
+mechanic1 = Mechanic.create!(name: "Morty Smith",
+                              years_of_experience: 11)
+
+mechanic2 = Mechanic.create!(name: "Michael Scott",
+                              years_of_experience: 15)
+
+mechanic3 = Mechanic.create!(name: "Gob Bluth",
+                              years_of_experience: 8)
