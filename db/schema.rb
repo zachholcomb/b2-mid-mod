003d3f19510e92a6f1fedbfc279d62c7ff2c0b7b
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-03 09:53:41 -0600

    Update ride spec to test for relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200403153612) do
+ActiveRecord::Schema.define(version: 20200403155045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20200403153612) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "mechanic_rides", force: :cascade do |t|
+    t.bigint "mechanic_id"
+    t.bigint "ride_id"
+    t.index ["mechanic_id"], name: "index_mechanic_rides_on_mechanic_id"
+    t.index ["ride_id"], name: "index_mechanic_rides_on_ride_id"
+  end
+
+  create_table "mechanics", force: :cascade do |t|
+    t.string "name"
+    t.integer "years_of_experience"
+  end
+
   create_table "rides", force: :cascade do |t|
     t.string "name"
     t.integer "thrill_rating"
@@ -31,5 +43,7 @@ ActiveRecord::Schema.define(version: 20200403153612) do
     t.index ["amusement_park_id"], name: "index_rides_on_amusement_park_id"
   end
 
+  add_foreign_key "mechanic_rides", "mechanics"
+  add_foreign_key "mechanic_rides", "rides"
   add_foreign_key "rides", "amusement_parks"
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Ride do
   describe 'relationships' do
     it {should belong_to :amusement_park}
+    it {should have_many :mechanic_rides}
+    it {should have_many(:mechanics).through(:mechanic_rides)}
   end
 end
